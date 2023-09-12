8612aab82e4afd3dc2628f0320e281b903e2c62d
deepak252 <trojanhorsedc@gmail.com>
2023-09-12 12:30:46 +0530

    Local Data Source Impl : Hive

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,6 +1,28 @@
+import 'package:bloc_app/core/error/exception.dart';
 import 'package:bloc_app/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 abstract class NumberTriviaLocalDataSource{
   Future<NumberTriviaModel> getLastNumberTrivia();
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource{
+  final numberTriviaBox = Hive.box('numberTriviaBox');
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia()async{
+    final numberTriviaJson = numberTriviaBox.get(0);
+    if(numberTriviaJson!=null){
+      return Future.value(NumberTriviaModel.fromJson(numberTriviaJson));
+    }else{
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache)async{
+    await numberTriviaBox.add(triviaToCache.toJson());
+  }
+
 }
