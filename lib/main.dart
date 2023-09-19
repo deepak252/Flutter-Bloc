cd4dfbdae39862f32ac5ac91d6de6e45dc1d68ad
deepak252 <trojanhorsedc@gmail.com>
2023-09-19 11:34:45 +0530

    MultiBloc : Cart, Counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:bloc_app/features/cart/cubit/cart_cubit.dart';
+import 'package:bloc_app/features/counter/cubit/counter_cubit.dart';
+import 'package:bloc_app/features/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +13,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(body: Text("Flutter App"),),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_)=>CounterCubit()),
+        BlocProvider(create: (_)=>CartCubit()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage(),
+      )
     );
+    // return MaterialApp(
+    //   title: 'Flutter Demo',
+    //   theme: ThemeData(
+    //     primarySwatch: Colors.blue,
+    //   ),
+    //   home: const HomePage(),
+    // );
   }
 }
 
